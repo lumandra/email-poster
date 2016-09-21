dd7a2e6df667b111bf0f14c4117ec856d08880c9
Marina Pecherina <lumandra7@gmail.com>
2016-09-22 01:48:05 +0300

    Show email images at table (#3)

--- app/helpers/reports_helper.rb
+++ app/helpers/reports_helper.rb
@@ -1,2 +1,21 @@
 module ReportsHelper
+  def user_images user
+    content_tag :div, class: 'row user-images' do
+      user.emails.each do |email|
+        email.attachments.each do |attach|
+          concat user_image attach.url
+        end
+      end
+    end
+  end
+
+  def user_image image_url
+    image_tag "https://process.filestackapi.com/AS6blLW1pSyaEm0kfBKmEz/output=format:jpg,density:30/#{image_url}", class: 'img-rounded'
+  end
+
+  private
+
+  def image_url image_path
+    request.protocol + request.host_with_port + image_path
+  end
 end
