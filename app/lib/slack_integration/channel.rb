d82e88a52b8fd3cd78c0848388b7febac7b41ec6
Marina Pecherina <lumandra7@gmail.com>
2016-11-09 15:50:41 +0300

    added the condition to display the Message in slack

--- app/lib/slack_integration/channel.rb
+++ app/lib/slack_integration/channel.rb
@@ -7,7 +7,11 @@ class SlackIntegration::Channel < SlackIntegration
   def create_notify
     images = @email.img_urls.map{|u| u['url']}.join(', ')
     @email.slack_channels.each do |channel|
-      message = "#{images}, Message: #{channel.slack_message_text}"
+      if channel.slack_message_text == nil || channel.slack_message_text == ''
+        message = "#{images}"
+      else
+        message = "#{images}, Message: #{channel.slack_message_text}"
+      end
       send_message(channel.webhook_url, message)
     end
   end
